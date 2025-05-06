0e3bb2c178961502e598be76c36f8c665f4582e7
izhans <izhansastre@gmail.com>
2025-05-06 13:36:26 +0200

    check if command is a route itself before searching its path

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 14:17:13 by isastre-          #+#    #+#             */
-/*   Updated: 2025/05/05 14:55:17 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/05/06 09:20:20 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,33 @@ int	main(int argc, char const *argv[], char *envp[])
 {
 	(void) argc, (void) argv;
 	// TODO check initial errors
-	
-	// ! encontrar primero los 2 comandos
-	// TODO !!! comprobar primero que se puede acceder a la ruta que viene como comando
-		// en ese caso no hay que buscar la ruta porque ya es un comando como tal
-	char *cmd_route = ft_find_commmand_route(argv[2], envp);
-	if (cmd_route == NULL)
+	if (argc != 5)
 	{
-		perror("Error:");
+		ft_putendl("./pipex infile cmd1 cmd2 outfile");
+		return (0);
+	}
+	// TODO separar comandos en cmd y args
+	char *cmd1 = ft_find_commmand_route(argv[2], envp);
+	char *cmd2 = ft_find_commmand_route(argv[3], envp);
+	if (cmd1 == NULL || cmd2 == NULL)
+	{
+		ft_putendl("Command not found");
+		return (0);
+	}
+	if (access(cmd1, X_OK) == -1 || access(cmd1, X_OK) == -1)
+	{
+		free(cmd1);
+		free(cmd2);
+		ft_putendl("Not enough permissions");
 		return (0);
 	}
 
 	return (0);
 }
 
+/**
+ * first checks if the cmd exists as a complete route, if so, a copy is returned
+ */
 char	*ft_find_commmand_route(char const *cmd, char **envp)
 {
 	char		**path;
@@ -44,6 +57,8 @@ char	*ft_find_commmand_route(char const *cmd, char **envp)
 	char		*cmd_route;
 	char const	*cmd_parts[] = {NULL, "/", cmd, NULL};
 
+	if (access(cmd, F_OK) == 0)
+		return (ft_strdup(cmd));
 	path = ft_get_path(envp);
 	if (path == NULL)
 		return (NULL); // ? o exit directamente?
@@ -51,15 +66,9 @@ char	*ft_find_commmand_route(char const *cmd, char **envp)
 	while (path[i])
 	{
 		cmd_parts[0] = path[i];
-		cmd_route = NULL;
 		ft_joinstrs(cmd_parts, &cmd_route);
-		printf("route: %s\n", cmd_route);
 		if (access(cmd_route, F_OK) == 0)
-		{
-			printf("command found\n");
-			ft_free_str_array(path);
-			return (cmd_route);
-		}
+			return (ft_free_str_array(path), cmd_route);
 		free(cmd_route);
 		i++;
 	}
